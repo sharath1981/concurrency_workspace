57a2e3bd9f394c6620f64890a9c6a506b3368bcf
SHARATH KUMAR B <sharath1981@gmail.com>
2021-03-14 01:08:13 +0530

    Refactored

--- advanced-concurrent-pattern-example/src/com/kpt/CyclicBarrierApp.java
+++ advanced-concurrent-pattern-example/src/com/kpt/CyclicBarrierApp.java
@@ -1,5 +1,6 @@
 package com.kpt;
 
+import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executors;
@@ -8,30 +9,33 @@ import java.util.stream.IntStream;
 
 public class CyclicBarrierApp {
 
-    private static final CyclicBarrier barrier = new CyclicBarrier(4, CyclicBarrierApp::print);
+    private static final CyclicBarrier barrier = new CyclicBarrier(4, CyclicBarrierApp::processAtBarrier);
+    private static final Random random = new Random(1); 
 
     public static void main(String[] args) throws Exception {
 
         final var service = Executors.newFixedThreadPool(4);
-        IntStream.rangeClosed(1, 4).forEach(i -> service.submit(() -> CyclicBarrierApp.printer(i)));
+        IntStream.rangeClosed(1, 4).forEach(i -> service.submit(CyclicBarrierApp::printer));
         service.shutdown();
     }
 
-    private static void printer(final int sleep) {
+    private static void printer() {
         try {
-            TimeUnit.SECONDS.sleep(sleep);
-            System.out
-                    .println(Thread.currentThread().getName() + " : waiting parties => " + barrier.getNumberWaiting());
-            System.out.println(Thread.currentThread().getName() + " : Barrier Broken ? => " + barrier.isBroken());
+            TimeUnit.SECONDS.sleep(random.nextInt(10));
+            System.out.println("     Number of parties reached Barrier(meeting point) => " + barrier.getNumberWaiting());
+            System.out.println("|---------------------------------------------------------------------------------------------|");
+            System.out.println("    "+Thread.currentThread().getName() + " : reached Barrier(meeting point)");
             barrier.await();
-            System.out.println(Thread.currentThread().getName() + " : proceeding after all parties reached at point ");
+            System.out.println("    "+Thread.currentThread().getName() + " : proceeding after all parties reached Barrier(meeting point) ");
         } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
     }
 
-    private static void print() {
-        System.out.println(Thread.currentThread().getName() + " : waiting parties => " + barrier.getNumberWaiting());
+    private static void processAtBarrier() {
+        System.out.println("     Number of parties reached Barrier(meeting point) => " + barrier.getNumberWaiting());
+        System.out.println("     All parties reached Barrier(meeting point)");
+        System.out.println("|---------------------------------------------------------------------------------------------|");
     }
 
 }
